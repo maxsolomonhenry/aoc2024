fc6c146cf89c667f7bd37249f08085ad97984515
Max Henry <mhenry@eers.ca>
2024-12-21 20:00:20 -0500

    wip doesnt work

--- src/day06/day06.cpp
+++ src/day06/day06.cpp
@@ -32,11 +32,62 @@ enum class Direction {
     Right
 };
 
+bool isBlocker(std::map<Point, char> grid, std::map<Point, Direction> path, Point guard, const int& maxX, const int& maxY) {
+    Direction direction = path[guard];
 
+    Point dxdy;
+    Direction target;
+    Direction opposite;
+    switch (direction)
+    {
+        case (Direction::Left):
+            // Search up.
+            dxdy = Point(0, -1);
+            target = Direction::Up;
+            opposite = Direction::Down;
+            break;
+        case (Direction::Up):
+            // Search right.
+            dxdy = Point(1, 0);
+            target = Direction::Right;
+            opposite = Direction::Left;
+            break;
+        case (Direction::Right):
+            // Search down.
+            dxdy = Point(0, 1);
+            target = Direction::Down;
+            opposite = Direction::Up;
+            break;
+        case (Direction::Down):
+            // Search left.
+            dxdy = Point(-1, 0);
+            target = Direction::Left;
+            opposite = Direction::Right;
+            break;
+    }
+
+    while (guard.x >= 0 && guard.x < maxX && guard.y >= 0 && guard.y < maxY)
+    {
+        guard += dxdy;
+        path[guard] = direction;
+        
+        if (grid[guard] == '#')
+        {
+            path[guard] = target;
+            return isBlocker(grid, path, guard, maxX, maxY);
+        }
+
+        if (path[guard] == target || path[guard] == opposite)
+            return true;
+    }
+
+    return false;
+}
 
 int main() {
     const auto lines = util::read("src/day06/input.txt");
     std::map<Point, char> grid;
+    std::map<Point, Direction> path;
 
     std::map<Direction, Point> step = {
         {Direction::Up, Point(0, -1)},
@@ -57,6 +108,7 @@ int main() {
 
     Point guard(-1, -1);
 
+    // Build grid from input.
     for (int x = 0; x < maxX; x++)
         for (int y = 0; y < maxY; y++)
         {
@@ -68,9 +120,12 @@ int main() {
     
     Direction direction = Direction::Up;
 
+    // Calculate.
+    int numBlockers = 0;
     while (guard.x >= 0 && guard.x < maxX && guard.y >= 0 && guard.y < maxY)
     {
         grid[guard] = 'X';
+        path[guard] = direction;
         Point target = guard + step[direction];
 
         if (grid[target] == '#')
@@ -84,7 +139,11 @@ int main() {
     {
         for (int y = 0; y < maxY; y++)
         {
-            if (grid[Point(x, y)] == 'X') numX++;
+            if (grid[Point(x, y)] == 'X')
+            {
+                numX++;
+                numBlockers += isBlocker(grid, path, Point(x, y), maxX, maxY);
+            }
             std::cout << grid[Point(x, y)];
         }
         std::cout << "\n";
@@ -92,6 +151,7 @@ int main() {
 
 
     std::cout << "Num visited squares: " << numX << '\n';
+    std::cout << "Num blockers: " << numBlockers << '\n';
 
     return 0;
 }
