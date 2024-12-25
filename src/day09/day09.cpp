60d6959c48f116bba50cdac2e434d392163517ec
Max Henry <mhenry@eers.ca>
2024-12-25 18:22:04 -0500

    new structure

--- src/day09/day09.cpp
+++ src/day09/day09.cpp
@@ -1,22 +1,26 @@
 #include "../common/util.h"
 
+#include <unordered_map>
 #include <iostream>
+#include <utility>
+
+using block = std::pair<int, int>;
 
 int main() {
+
+
     const auto& lines = util::read("src/day09/input.txt");
 
-    std::vector<int> nBlocks;
-    std::vector<int> nEmpty;
+    std::unordered_map<int, block> memory;
 
+    int fileNo = 0;
     for (int i = 0; i < lines[0].size(); i++)
     {
         if (i % 2 == 0)
-            nBlocks.push_back(util::ctoi(lines[0][i]));
+            memory[i] = block(fileNo++, util::ctoi(lines[0][i]));
         else
-            nEmpty.push_back(util::ctoi(lines[0][i]));
+            memory[i] = block(-1, util::ctoi(lines[0][i]));
     }
-
-    
 
     return 0;
 }
